8361ef221456cf51a65f1acfd11200fb1bdaa9d7
Farhan Ahmad <112857049+thefarhanahmad@users.noreply.github.com>
2023-07-17 00:36:45 +0530

    Update Product.jsx
    product component changed

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -20,8 +20,8 @@ const Product = ({ item }) => {
   };
 
   return (
-    <div className="bg-white hover:shadow-2xl  rounded-md hover:scale-105 transition-all duration-300 shadow-lg lg:w-[30%] h-[50%] md:w-[40%] sm:w-[80%] overflow-hidden box-border">
-      <div className="flex justify-center flex-col items-center gap-2 p-5 box-border">
+    <div className="bg-white flex items-center hover:shadow-2xl pb-8 relative mb-4 rounded-md hover:scale-105 transition-all duration-300 shadow-lg lg:w-[24%]  md:w-[40%] sm:w-[80%] overflow-hidden box-border">
+      <div className="flex justify-center flex-col items-center gap-2  p-5  box-border">
         <h2 className="text-lg font-bold">
           {item.title.length > 15
             ? item.title.substring(0, 20) + " ..."
@@ -32,17 +32,17 @@ const Product = ({ item }) => {
             ? item.description.substring(0, 105) + " ..."
             : item.description}
         </p>
-        <div className="w-44 h-44">
+        <div className="w-44 h-44 overflow-hidden">
           <img
             src={item.image}
             alt="img"
             className="w-full h-full object-contain"
           />
         </div>
-        <div className="flex justify-between items-center w-full">
+        <div className="flex gap-14 justify-center items-center w-full absolute bottom-3">
           <p className="text-green-700 font-bold text-sm">${item.price}</p>
 
-          {cart.some((p) => p.id == item.id) ? (
+          {cart.some((p) => p.id === item.id) ? (
             <button
               className="text-gray-700 border-2 border-gray-700 rounded-full font-semibold 
           text-[12px] p-1 px-3 uppercase 
